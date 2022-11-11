ed4e7dbbd51cf0a7c18a2d36f6ca0c303466b066
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2022-11-11 14:48:35 +0530

    Add files via upload

---
+++ 06_typeConversions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 8;
+    float b = 9.67;
+    char c = 'c';
+    int d = 68;
+
+    cout<<"Float a is: "<<float(a)<<endl;
+    cout<<"Int b is: "<<int(b)<<endl;
+    cout<<"Int c is: "<<int(c)<<endl;
+    cout<<"Char d is: "<<char(d)<<endl;
+    return 0;
+}
