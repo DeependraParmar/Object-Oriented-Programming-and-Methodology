8e236b957228de7720c220f31b0ac34cc6934b7d
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2022-11-11 14:46:46 +0530

    Add files via upload

---
+++ 01_helloWorld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the first number: ";
+    cin>>a;
+    cout<<"Enter the second number: ";
+    cin>>b;
+
+    cout<<endl;
+    cout<<"Sum is: "<<(a+b)<<endl;
+    cout<<"Difference is: "<<(a-b)<<endl;
+    cout<<"Product is: "<<(a*b)<<endl;
+    cout<<"Quotient is: "<<(a/b)<<endl;
+    cout<<"Remainder is: "<<(a%b)<<endl;
+    
+    return 0;
+}
